e51ebab7ba6dce32cc56c3457032591d4c84d8b5
Yashin401 <yashin000@gmail.com>
2025-06-22 10:54:08 +0530

    Added task manager api routes
    Added task manager api routes for all the CRUD operations

---
+++ routes/tasks.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const router = express.Router();  
+const initialTasks = require('../task.json').tasks;
+
+const tasks = [...initialTasks]
+
+router.get('/', (req, res) => {
+    const completedQueryParam = req.query.completed; 
+    let tasksToReturn = tasks;
+    if (completedQueryParam !== undefined) {
+        const filterCompletedBoolean = completedQueryParam.toLowerCase() === 'true';
+        tasksToReturn = tasks.filter(item => item.completed === filterCompletedBoolean); 
+    }
+    res.status(200).json(tasksToReturn);
+});
+
+router.get('/:id', (req,res) =>{
+    const taskId = req.params.id
+    const task = tasks.find((item) => item.id == taskId )
+    if(task){
+        res.status(201).json(task)
+    }else {
+        res.status(404).json({message:"Course Not Found"})
+    }
+})
+
+router.post('/', (req,res) =>{
+    const newTask = req.body
+
+    if(!newTask || !newTask['title'] || !newTask['description'] || typeof(newTask['completed']? newTask['completed'] : false) !== "boolean"){
+        res.status(400).json({
+            message: 'Bad Request: Invalid Data.'
+        })
+    }
+
+    const idGenerated = (tasks.length +1)
+    newTask.id= idGenerated
+    tasks.push(newTask)
+    res.status(201).json({message:'Created successfully'})
+})
+
+router.put('/:id', (req,res) =>{
+    const taskId = req.params.id
+    const newTask = req.body
+
+    if(!newTask || !newTask['title'] || !newTask['description'] || typeof(newTask['completed']? newTask['completed'] : false) !== "boolean"){
+        res.status(400).json({
+            message: 'Bad Request: Invalid Data.'
+        })
+    }
+    const taskIndex = tasks.findIndex((item) => item.id == taskId )
+    if(taskIndex != -1){
+        tasks[taskIndex] = {
+            ...tasks[taskIndex],
+            ...newTask,
+            completed: newTask.completed ? true: false,
+            id: tasks[taskIndex]['id']
+        }
+        res.status(200).json({message:`Updated datad for ${taskId}`})
+    } else {
+        res.status(404).json({message:"Course not found"})
+    }
+})
+
+router.delete('/:id', (req,res)=>{
+    const taskId = req.params.id
+    const taskIndex = tasks.findIndex(item => item.id == taskId)
+
+    if(taskIndex != -1){
+        tasks.splice(taskIndex,1)
+        res.send(tasks)
+    }else {
+        res.status(404).json({message:"Course not found"})
+    }
+})
+
+module.exports = router
